4ecc3041be17079d8a554d1b6bbe94d3065a14bf
a.navrotskiy <navrotskiy@corp.mail.ru>
2015-11-05 17:32:18 +0300

    init

---
+++ InstancedProperty/Source/InstancedProperty/InstancedPropertyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "InstancedPropertyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INSTANCEDPROPERTY_API AInstancedPropertyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
